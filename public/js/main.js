cdbf71537c56c877d6ea508bdc88d761d9106345
Firomsa Mahamud <MFiromsa@gmail.com>
2023-03-28 15:19:55 -0500

    add pop notification for weekly events

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,17 @@
+/* Toggle between adding and removing the "responsive" class to nav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myNav");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+}
+
+
 const carouselContainers = [...document.querySelectorAll('.carousel-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
-
 
 //carousel arrow to change images
 carouselContainers.forEach((item, i) => {
@@ -16,3 +26,11 @@ carouselContainers.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 })
+
+// Get all elements with class="closebtn"
+var close = document.getElementById("closebtn");
+close.addEventListener('click', closeBtn)
+
+function closeBtn(){
+  this.parentElement.style.display='none';
+}
