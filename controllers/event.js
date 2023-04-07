1292567a5842a161084f5cb70cdf7b63f0235230
Firomsa Mahamud <MFiromsa@gmail.com>
2023-04-07 05:42:37 -0500

    added calender

--- controllers/event.js
+++ controllers/event.js
@@ -14,7 +14,7 @@ module.exports = {
     getFeed: async (req, res) => {
         try {
             const event = await Event.find().sort({ date: 1 }).lean();
-            res.render('feed.ejs', {event: event, user: req.user})
+            res.render('calendar.ejs', {event: event, user: req.user})
         } catch (error) {
             console.log(error)
         }
